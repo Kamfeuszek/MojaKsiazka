5d3b24cb93738aac5b2b55da7199010be5928451
Kamfeuszek <florczakk01@gmail.com>
2025-04-08 11:35:13 +0200

    hotfix3

--- app/src/main/java/com/example/mojaksiazka/MainActivity.java
+++ app/src/main/java/com/example/mojaksiazka/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
     public static void createNotificationChannel(Context context) {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
             NotificationChannel channeldefault = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
 
             if (notificationManager != null) {
